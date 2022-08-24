f1bfbc41bbf3d421bd33ee55ca22d01e29c1fabf
Balazs Radvanyi <balazs@riltech.co>
2022-08-24 13:36:23 +0200

    wip(white-label): svg usage causes a buildtime error"

--- sample/src/main/java/com/github/paylike/sample/SampleActivity.kt
+++ sample/src/main/java/com/github/paylike/sample/SampleActivity.kt
@@ -3,14 +3,17 @@ package com.github.paylike.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.paylike.sample.ui.theme.Kotlin_sdkTheme
+import com.steliospapamichail.creditcardmasker.viewtransformations.CardNumberMask
 
 class SampleActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +25,43 @@ class SampleActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    WhiteLabelDemo()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalAnimationGraphicsApi::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun WhiteLabelDemo() {
+    CardNumber()
+    val visaIcon = AnimatedImageVector.animatedVectorResource(R.drawable.visa_icon)
+
+    Icon(
+        painter = rememberAnimatedVectorPainter(visaIcon, false),
+        contentDescription = null
+    )
+}
+
+
+@Composable
+fun CardNumber() {
+    var number by remember { mutableStateOf("") }
+    TextField(
+        value = number,
+        visualTransformation = CardNumberMask(),
+        onValueChange = {
+            if (it.length <= 16) number  = it
+        }
+    )
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Kotlin_sdkTheme {
-        Greeting("Android")
+        WhiteLabelDemo()
     }
 }
