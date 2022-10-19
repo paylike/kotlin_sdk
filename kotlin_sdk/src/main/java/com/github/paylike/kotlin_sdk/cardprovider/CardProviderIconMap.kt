3d42518ae28e459fac824c1b04172a13e608db73
Laszlo Kocsis <kocsislaci95@gmail.com>
2022-10-19 13:57:51 +0200

    fix(sdk): card provider icon map asset naming

--- kotlin_sdk/src/main/java/com/github/paylike/kotlin_sdk/cardprovider/CardProviderIconMap.kt
+++ kotlin_sdk/src/main/java/com/github/paylike/kotlin_sdk/cardprovider/CardProviderIconMap.kt
@@ -6,7 +6,7 @@ import com.github.paylike.kotlin_sdk.R
 val CardProviderIconMap: Map<SupportedCardProviders, Int?> =
     mapOf(
         SupportedCardProviders.NONE to null,
-        SupportedCardProviders.MAESTRO to R.drawable.ic_maestro_icon,
-        SupportedCardProviders.MASTERCARD to R.drawable.ic_mastercard_icon,
-        SupportedCardProviders.VISA to R.drawable.ic_visa_icon,
+        SupportedCardProviders.MAESTRO to R.drawable.ic_maestro,
+        SupportedCardProviders.MASTERCARD to R.drawable.ic_mastercard,
+        SupportedCardProviders.VISA to R.drawable.ic_visa,
     )
