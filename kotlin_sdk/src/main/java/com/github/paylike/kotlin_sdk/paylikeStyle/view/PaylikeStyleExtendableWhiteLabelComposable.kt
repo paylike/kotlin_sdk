5bbce46eb263cc516c5aeff7f5d0403036ad2fb7
Laszlo Kocsis <kocsislaci95@gmail.com>
2022-10-31 14:20:57 +0100

    feat(paylikeStyelForms): implemented forms

--- kotlin_sdk/src/main/java/com/github/paylike/kotlin_sdk/paylikeStyle/view/PaylikeStyleExtendableWhiteLabelComposable.kt
+++ kotlin_sdk/src/main/java/com/github/paylike/kotlin_sdk/paylikeStyle/view/PaylikeStyleExtendableWhiteLabelComposable.kt
@@ -1,7 +1,11 @@
 package com.github.paylike.kotlin_sdk.paylikeStyle.view
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.github.paylike.kotlin_sdk.*
 import com.github.paylike.kotlin_sdk.theme.PaylikeTheme
 import com.github.paylike.kotlin_sdk.whitelabel.extendable.view.ExtendableWhiteLabelComposable
 import com.github.paylike.kotlin_sdk.whitelabel.extendable.viewmodel.ExtendableWhiteLabelViewModel
@@ -12,8 +16,39 @@ fun PaylikeStyleExtendableWhiteLabelComposable(
     modifier: Modifier = Modifier,
     viewModel: ExtendableWhiteLabelViewModel,
 ) {
-    ExtendableWhiteLabelComposable(
-        viewModel = viewModel,
-        theme = { content -> run { PaylikeTheme() { content.invoke() } } },
-    )
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        PaylikeTheme {
+            if (viewModel.paymentFormState.isSuccess) {
+                SuccessAnimation(
+                    modifier = Modifier.fillMaxWidth().aspectRatio(2f),
+                )
+            }
+            if (viewModel.paymentFormState.isError) {
+                ErrorLog(
+                    modifier = Modifier.fillMaxWidth(),
+                    error = viewModel.paymentFormState.error!!,
+                )
+            }
+            if (!viewModel.paymentFormState.isSuccess) {
+                ExtendableWhiteLabelComposable(
+                    viewModel = viewModel,
+                    theme = { content -> run { PaylikeTheme() { content.invoke() } } },
+                )
+            }
+            if (viewModel.paymentFormState.isPaymentFlowInitiated) {
+                LoadingSpinner(
+                    modifier = Modifier.size(32.dp),
+                    color = PaylikeTheme.colors.primary,
+                )
+            }
+            SecurePaymentLabel(
+                modifier = Modifier.fillMaxWidth().height(50.dp),
+                textStyle = PaylikeTheme.typography.caption,
+            )
+        }
+    }
 }
--- kotlin_sdk/src/main/java/com/github/paylike/kotlin_sdk/paylikeStyle/view/PaylikeStyleSimpleWhiteLabelComposable.kt
+++ kotlin_sdk/src/main/java/com/github/paylike/kotlin_sdk/paylikeStyle/view/PaylikeStyleSimpleWhiteLabelComposable.kt
@@ -5,9 +5,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.github.paylike.kotlin_sdk.LoadingSpinner
-import com.github.paylike.kotlin_sdk.SecurePaymentLabel
-import com.github.paylike.kotlin_sdk.SuccessAnimation
+import com.github.paylike.kotlin_sdk.*
 import com.github.paylike.kotlin_sdk.theme.PaylikeTheme
 import com.github.paylike.kotlin_sdk.whitelabel.simple.view.WhiteLabelComposable
 import com.github.paylike.kotlin_sdk.whitelabel.simple.viewmodel.WhiteLabelViewModel
@@ -19,25 +17,29 @@ fun PaylikeStyleSimpleWhiteLabelComposable(
     viewModel: WhiteLabelViewModel,
 ) {
     Column(
-        modifier = modifier.fillMaxWidth().imePadding(),
+        modifier = modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         PaylikeTheme {
-            if (viewModel.paymentFormState.isFinished) {
+            if (viewModel.paymentFormState.isSuccess) {
                 SuccessAnimation(
                     modifier = Modifier.fillMaxWidth().aspectRatio(2f),
                 )
             }
-            if (!viewModel.paymentFormState.isFinished) {
+            if (viewModel.paymentFormState.isError) {
+                ErrorLog(
+                    modifier = Modifier.fillMaxWidth(),
+                    error = viewModel.paymentFormState.error!!,
+                )
+            }
+            if (!viewModel.paymentFormState.isSuccess) {
                 WhiteLabelComposable(
                     viewModel = viewModel,
                     theme = { content -> run { PaylikeTheme { content.invoke() } } }
                 )
             }
-            if (
-                !viewModel.paymentFormState.isFinished && !viewModel.paymentFormState.isInitialState
-            ) {
+            if (viewModel.paymentFormState.isPaymentFlowInitiated) {
                 LoadingSpinner(
                     modifier = Modifier.size(32.dp),
                     color = PaylikeTheme.colors.primary,
